a588ed6c86422ad1ca26bea79c0c917e27ea39f7
jeffli817 <jeffli8172@gmail.com>
2022-03-24 15:02:46 +0800

    Action Creator Thunk

--- Section 19 Practice/src/store/cart-slice.js
+++ Section 19 Practice/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -17,28 +18,71 @@ const cartSlice = createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
       } else {
-        existingItem.quantity +=1;
+        existingItem.quantity += 1;
         existingItem.totalPrice += newItem.price;
       }
       state.totalQuantity++;
     },
-    removeItemFromCart(state,action) {
-        const id = action.payload;
-        const existingItem = state.items.find((item) => item.id === id);
-        if (existingItem.quantity ===1){
-            state.items = state.items.filter((item) => item.id !==id);
-        }
-        else{
-            existingItem.quantity--;
-            existingItem.totalPrice -= existingItem.price;
-        }
-        state.totalQuantity--;
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+      state.totalQuantity--;
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-js-http-9116d-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("sending cart data failed");
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sending cart data successfully",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
