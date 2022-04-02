6bbe5f5afb8ab249e2ecacaf408bcabd15a13b10
jeffli817 <jeffli8172@gmail.com>
2022-04-02 12:26:17 +0800

    proj

---
+++ Section 20 - projects/src/pages/AllQuote.js
@@ -0,0 +1,17 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES =[
+        {id:'q1', author:'Max', text:'Learning React is fun !'},
+        {id:'q2', author:'Min', text:'Learning React is great !'},
+]
+
+
+
+const AllQuote = ()=>{
+    return(
+        <QuoteList quotes={DUMMY_QUOTES}/>
+        
+    )
+}
+
+export default AllQuote;
